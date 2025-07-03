aa9c0c06afb7e1b8458c921eee61338c1eae371f
Basic-beep <elliottroseemail@gmail.com>
2025-07-02 22:37:23 -0400

    menu update
    worked on adding a menu, it doesn't lead anywhere yet, but I'll work on that tomorrow

---
+++ GenPass.py
@@ -0,0 +1,30 @@
+""" Welcome to
+   ____            ____               
+  / ___| ___ _ __ |  _ \ __ _ ___ ___ 
+ | |  _ / _ \ '_ \| |_) / _` / __/ __|
+ | |_| |  __/ | | |  __/ (_| \__ \__ \
+  \____|\___|_| |_|_|   \__,_|___/___/
+
+  Since you're here in the code, if you find any bugs or can think of improvements, DM me on Slack @ElliottRose                                 
+"""
+
+import string
+adj = ('A', 'Abandoned','Abdominal','Abhorrent','Abiding','Abject','Able','Able-Bodied','Abnormal','Abounding'
+       ,'Babbling','Baby','Background','Backhanded','Bacterial','Bad','Bad-tempered','Baffled','Baffling','Bald','Balding'
+       ,'Cackling','Caged','Cagey','Calculable','Calculated','Calculating','Callous','Calm','Calming','Camouflaged','Cancelled'
+       ,'Daffy','Daft','Daily','Dainty','Damaged','Damaging','Damp','Dandy','Dangerous','Dapper'
+       ,'Eager','Early','Earnest','Ear-piercing','Ear-splitting','Earthshaking','Earthy','Eastern','Eccentric','Edgy','Eerie'
+       ,'Fabled','Fabulous','Facetitious','Factual','Faded','Fading','Failed','Fair','Faithful','Faithless'
+       ,'Gallent','Gaping','Garbled','Gargantuan','Generous','Generic','Gaseous','Giant','Ghostly','Ghastly'
+       ,'Hairy','Handsome','Harmful','Harmless','Hated','Hateful','Hazardous','Helpful','Helpless','Horrid'
+       ,'Icky','Ideal','Impolite','Inclusive','Incorruptable','Indestructible','Indirect','Inevitable','Infamous','Innocent'
+       ,'Jaded','Jealous','Jolly','Joyful','Joyless','Juicy','Jumpy','Juvenile','Jubilant','Jerky'
+       ,'Kind','Knowledgeable','Kingly','Keen','Kaput','Kindhearted','Key','Knowing','Knobby','Knightly'
+       ,'Lacy','Lame','Large','Large','Larger','Largest','Last','Lasting','Later','Latest'
+       ,'Macabre','Macho','Magic','Magical','Main','Magenta','Majestic','Mandatory','Married','Massive'
+       ,'Naive','Nameless','N','','','','','','','')
+
+Response = input('Welcome to GenPass! ' 
+'If you want a new Password press "N" for new. ' 
+'If you want to see passwords you have saved this session press "S" for saved. ' 
+'If you want to end the session press "E" for End.')
